54e35c952b43f9156ed80d395bdc137c9f5ff7c1
wildantaufik354 <wildantaufik86@gmail.com>
2024-11-25 22:57:51 +0700

    feat: singup api testing

---
+++ backend/src/app.ts
@@ -0,0 +1,27 @@
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+import userRoutes from "./routes/users";
+import createHttpError, { isHttpError } from "http-errors";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+
+app.use((req, res, next) => {
+  next(createHttpError(404, "Endpoint Not Found!"));
+});
+
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error(error);
+  let errorMessage = "An unknown error occured";
+  let statusCode = 500;
+  if (isHttpError(error)) {
+    statusCode = error.status;
+    errorMessage = error.message;
+  }
+  res.status(statusCode).json({ error: errorMessage });
+});
+
+export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,14 @@
-import express from 'express';
-const app = express();
-const port = 5000;
+import app from "./app";
+import env from "./utils/validatEnv";
+import mongoose from "mongoose";
 
-app.get('/', (req, res) => {
-  res.send('Hellow World');
-});
-
-app.listen(port, () => {
-  console.log('server running on port: ' + port);
-});
+const port = env.PORT;
+mongoose
+  .connect(env.MONGO_CONNECTION_STRING)
+  .then(() => {
+    console.log("Mongoose Connected");
+    app.listen(port, () => {
+      console.log("server running on port: " + port);
+    });
+  })
+  .catch(console.error);

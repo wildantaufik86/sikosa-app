ee80f6969783fb860adbd19b0c7619eeef8bc027
wildantaufik354 <wildantaufik86@gmail.com>
2024-12-12 23:51:38 +0700

    fix: refresh token and password login bug

--- backend/src/utils/bcrypt.ts
+++ backend/src/utils/bcrypt.ts
@@ -3,5 +3,7 @@ import bcrypt from "bcrypt";
 export const hashValue = async (value: string, saltRounds?: number) =>
   bcrypt.hash(value, saltRounds || 10);
 
-export const compareValue = async (value: string, hashedValue?: number) =>
-  bcrypt.hash(value, hashedValue || 10).catch(() => false);
+export const compareValue = async (value: string, hashedValue?: string) => {
+  if (!hashedValue) return false;
+  return bcrypt.compare(value, hashedValue);
+};
--- backend/src/utils/cookies.ts
+++ backend/src/utils/cookies.ts
@@ -1,6 +1,7 @@
 import { CookieOptions, Response } from "express";
 import { fifteenMinutesFromNow, thirtyDaysFromNow } from "./date";
 
+export const REFRESH_PATH = "/auth/refresh";
 const secure = process.env.NODE_ENV !== "Development";
 
 const defaults: CookieOptions = {
@@ -30,3 +31,8 @@ export const setAuthCookies = ({ res, accessToken, refreshToken }: Params) =>
   res
     .cookie("accessToken", accessToken, getAccessTokenCookieOptions())
     .cookie("refreshToken", refreshToken, getRefreshTokenCookieOptions());
+
+export const clearAuthCookies = (res: Response) =>
+  res
+    .clearCookie("accessToken")
+    .clearCookie("refreshToken", { path: REFRESH_PATH });

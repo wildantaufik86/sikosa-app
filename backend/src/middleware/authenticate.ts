bbf13e9515d2aaf9e23d818ce4e2dbb506d64ede
wildantaufik354 <wildantaufik86@gmail.com>
2024-12-14 18:12:48 +0700

    setting up profile update

--- backend/src/middleware/authenticate.ts
+++ backend/src/middleware/authenticate.ts
@@ -3,6 +3,7 @@ import appAssert from "../utils/appAssert";
 import AppErrorCode from "../constants/appErrorCode";
 import { UNAUTHORIZED } from "../constants/http";
 import { verifyToken } from "../utils/jwt";
+import mongoose from "mongoose";
 
 // wrap with catchErrors() if you need this to be async
 const authenticate: RequestHandler = (req, res, next) => {
@@ -21,9 +22,26 @@ const authenticate: RequestHandler = (req, res, next) => {
     error === "jwt expired" ? "Token expired" : "Invalid token",
     AppErrorCode.InvalidAccessToken
   );
+  // Validasi payload userId dan sessionId yang diharapkan adalah string
+  if (
+    typeof payload.userId === "string" &&
+    typeof payload.sessionId === "string"
+  ) {
+    try {
+      // Cast ke ObjectId jika valid
+      req.userId = new mongoose.Types.ObjectId(payload.userId);
+      req.sessionId = new mongoose.Types.ObjectId(payload.sessionId);
+    } catch (err) {
+      return res.status(UNAUTHORIZED).json({
+        message: "Invalid ObjectId format in token",
+      });
+    }
+  } else {
+    return res.status(UNAUTHORIZED).json({
+      message: "Invalid token payload",
+    });
+  }
 
-  // req.userId = payload.userId;
-  // req.sessionId = payload.sessionId;
   next();
 };
 

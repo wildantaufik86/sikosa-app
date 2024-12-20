0154855f7790319ccf8959c7af7027a920abe431
Muhammad Habi <muhammadhabi01@gmail.com>
2024-12-20 21:56:23 +0700

    chore: update artikel user

--- frontend/src/components/user/components/artikel/ArtikelCard.jsx
+++ frontend/src/components/user/components/artikel/ArtikelCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import CONFIG from "../../../../config/config";
 import { formattedDate, formattedTitle } from "../../../../utils/utils";
+import parse from "html-react-parser";
 
 const ArtikelCard = ({ article }) => {
   return (
@@ -19,6 +20,9 @@ const ArtikelCard = ({ article }) => {
 
         <div className="px-4 py-2 flex-1 mt-4">
           <h3 className="text-sm font-semibold">{formattedTitle(article.title)}</h3>
+        </div>
+        <div className="px-4 py-2 flex-1">
+          <p className="text-sm font-normal line-clamp-2">{parse(article.content)}</p>
         </div>
         <div className="flex flex-col px-4 py-2">
           <p className="text-[8px] font-light">{formattedDate(article.createdAt)}</p>

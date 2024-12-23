cad75cea5d0423fa5c4277ca9e178cfeae454a31
wildantaufik354 <wildantaufik86@gmail.com>
2024-12-24 00:37:59 +0700

    api fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -45,7 +45,7 @@ app.use("/api/v1", articleRoutes);
 // api konsul
 app.use("/consultation", consultationRoutes, userRoutes);
 
-app.use("/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.use("/user", authenticate, userRoutes);
 app.use("/psikolog", authenticate, psikologRoutes);

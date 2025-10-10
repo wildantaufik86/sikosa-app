d3ccc219f196644ad4b714364e58837858829aca
MuhammadAidil-dev <muhammad.aidil09767@gmail.com>
2025-10-10 15:01:55 +0700

    chor: setup to testing

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,17 +1,8 @@
 import { z } from "zod";
 import catchErrors from "../utils/catchErrors";
-import {
-  createAccount,
-  loginUser,
-  refreshUserAccessToken,
-} from "../services/auth.service";
+import { createAccount, loginUser, refreshUserAccessToken } from "../services/auth.service";
 import { CREATED, OK, UNAUTHORIZED } from "../constants/http";
-import {
-  clearAuthCookies,
-  getAccessTokenCookieOptions,
-  getRefreshTokenCookieOptions,
-  setAuthCookies,
-} from "../utils/cookies";
+import { clearAuthCookies, getAccessTokenCookieOptions, getRefreshTokenCookieOptions, setAuthCookies } from "../utils/cookies";
 import { loginSchema, registerSchema } from "./auth.schemas";
 import { verifyToken } from "../utils/jwt";
 import SessionModel from "../models/sessionModel";
@@ -28,7 +19,7 @@ export const registerHandler = catchErrors(async (req, res) => {
 
   return setAuthCookies({ res, accessToken, refreshToken })
     .status(CREATED)
-    .json(user);
+    .json({ status: "success", message: "Berhasil membuat akun", data: user });
 });
 
 export const loginHandler = catchErrors(async (req, res) => {
@@ -56,18 +47,14 @@ export const logoutHandler = catchErrors(async (req, res) => {
   }
 
   // clear cookies
-  return clearAuthCookies(res)
-    .status(OK)
-    .json({ message: "Logout successful" });
+  return clearAuthCookies(res).status(OK).json({ message: "Logout successful" });
 });
 
 export const refreshHandler = catchErrors(async (req, res) => {
   const refreshToken = req.cookies.refreshToken as string | undefined;
   appAssert(refreshToken, UNAUTHORIZED, "Missing refresh token");
 
-  const { accessToken, newRefreshToken } = await refreshUserAccessToken(
-    refreshToken
-  );
+  const { accessToken, newRefreshToken } = await refreshUserAccessToken(refreshToken);
   if (newRefreshToken) {
     res.cookie("refreshToken", newRefreshToken, getRefreshTokenCookieOptions());
   }
